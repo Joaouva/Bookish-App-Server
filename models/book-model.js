34a9dd4c54930dbed0d6ed92ac48eb43893c0ebb
Diogo Pascoal <dmtpascoal@gmail.com>
2020-12-03 16:39:39 +0000

    .

--- models/book-model.js
+++ models/book-model.js
@@ -1,23 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema(
-  {
-    Title: String,
-    Author: String,
-    Publisher: String,
-    Published: Date,
-    ISBN: Number,
-    Description: String,
-    Language: String,
-    Image: String,
-    Price: Number,
-    Grade: {
-            type: String,
-            enum: ['A', 'B', 'C'],
-        }
-  }  
-);
+const bookSchema = new Schema({
+  title: String,
+  author: String,
+  publisher: String,
+  published: Date,
+  ISBN: Number,
+  description: String,
+  language: String,
+  image: String,
+  price: Number,
+  grade: {
+    type: String,
+    enum: ["A", "B", "C"],
+  },
+});
 
 const Book = mongoose.model("Book", bookSchema);
-module.exports = Book
+module.exports = Book;
