66fc5f863626f066634a418bd66a45b0c99e1fe9
Joao Uva <joaouva@gmail.com>
2020-12-10 15:01:19 +0000

    deploy the front-end

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,14 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.all("*", (req, res, next) => {
+	if (req.originalUrl.startsWith("/api")) {
+		// skip any /api routes
+		next();
+	} else {
+		res.sendFile(path.join(__dirname, "../dist/index.html"));
+	}
+});
+
 module.exports = router;
 
